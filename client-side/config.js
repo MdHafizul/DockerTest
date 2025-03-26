1db49ce6febf447d93b1368cfe169063a39fcbf5
Hafizul Rahman <hafizulrahman011@gmail.com>
2025-03-26 12:52:04 +0800

    finalized

--- client-side/config.js
+++ client-side/config.js
@@ -1,4 +1,4 @@
 // This file exposes configuration values to the client-side application
 const config = {
-    API_BASE_URL: 'http://server:3000' // Update to use the server container name
+    API_BASE_URL: 'http://localhost:3000' // Use localhost to access the backend from the host machine
 };
